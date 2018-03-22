735c810c14614786d7bdefe76e00453390920f13
Monica <mntamaru@gmail.com>
2018-03-21 22:07:51 -0700

    getting closer on addresses and info blurbs

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -2,38 +2,27 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-class EaterSFHeatmap::Restaurant
+class Restaurant
 
 	attr_accessor :name, :address, :phone, :blurb
 
-	def initialize
-		# an array of Restaurant objects
-		@restaurants = []
-	end
-
-
 	def self.list_item 
+		self.scrape
 		
-		@restaurants.each.with_index do |restaurant, index|
-
-			puts "#{index + 1}.   #{restaurant.name}"
-
-		end   
+		# doc = Nokogiri::HTML(open("https://sf.eater.com/maps/best-new-restaurants-san-francisco-oakland-berkeley-heatmap"))
+		
+		# list = doc.css(".c-mapstack__cards")
+		# list.css("h2").text
+		# "Where to eat right now\n1 Funky Elephant\n2 Cdp\n3 dosa By DOSA\n4Eight Tables by George Chen\n5 The SnugSHN Orpheum Theatre\n6 International Smoke\n7 IPPUDO San Francisco\n8 Villon\n9 Nyum Bai\n10 Kaya Restaurant\n11 Barvale\n12 True Laurel\n13 Son’s AdditionRelated Maps"
+		# need to split on \n, add each entry into an array
 	end
 
 	def self.scrape
 		#scrape Eater SF and return restaurants based on that data
-
-		@restaurants << self.scrape_heatmap
-
-
 		
+		restaurants= []
 
-		restaurants
-	end
-
-	def self.scrape_heatmap
-
+		restaurants << self.scrape_heatmap
 		# 1. Go to https://sf.eater.com/maps/best-new-restaurants-san-francisco-oakland-berkeley-heatmap
 
 		# 2. Extract the properties
@@ -49,65 +38,76 @@ class EaterSFHeatmap::Restaurant
 
 		# 3. instantiate a Restaurant object
 
+		restaurants
+	end
+
+	def self.scrape_heatmap
 		doc = Nokogiri::HTML(open("https://sf.eater.com/maps/best-new-restaurants-san-francisco-oakland-berkeley-heatmap"))
-		list = doc.css(".c-mapstack__cards")
 		
-		# b. and c. - Has all addresses AND phone numbers - each line item has one address and one phone number.
-	    address = list.css(".c-mapstack__address").text
-	    addresses = address.split("\n")
+		list = doc.css(".c-mapstack__cards")
 
-	    adds = []
-	    phones = []
-	    hold = []
+    #address[0].children.text = "address"
+    #address[1].children.text = "phone \n"
+    
+    address = list.css(".c-mapstack__address")
+    adds = []
+    phones = []
+    hold_add = []
 
-	    addresses.each.with_index do |line, i|
-	      hold = line.split(/(?=\()/)
-	      adds << hold[0]
-	      phones << hold[1]
-	    end 
-
-
-
-
-		holding = []
-
+    address.each do |add|
+      if add.children.text != nil
+        hold_add << add
+      end
+    end
+	
+	  hold_add.each.with_index do |info, i|
+	    if info.text != nil
+	      adds << info.text
+	    end
+	  end
+		
+	# all info separated, but how to match address to ph number when one doesn't have a phone?
+	
+	
+		
+  # For restaurant names
+    hold_name = []
 		cards = list.css("h2")
 		cards.each do |sect|
-		# cards = list.css("h2 .c-mapstack__card-index").text gives you array of numbers, which corresponds to the list number	
 		  if sect.css("h2 .c-mapstack__card-index").text != nil
-		    holding << sect
+		    hold_name << sect
 		  end
 		end
 		
-		# holding[1].css(".c-mpstack__card-index").text = "1"
-		# holding[1].children[2].text = "Funky Elephant"
+		names = []
 		
-         binding.pry
+		hold_name.each.with_index do |name, index|
+		  if name.children[2] != nil
+		    names << name.children[2].text
+		  end
+		end
+		
+		# the array "names" holds the restaurant names
 
+    
+    
+    
+    # For info blurbs
+    blurb = list.css(".c-entry-content")
+    hold_blurb = []
+    blurb.each do |sect|
+		  if sect.children != nil
+		    hold_blurb << sect.children.text
+		  end
+		end
 
-	    # See above method for this issue
-
-	    number = list.css(".c-mapstack__card-index").text
-	    names = list.css(".c-mapstack__card-index span").text
-
-	    # this grabs ALL the blurbs - how to split up?
-
-	    blurb = list.css(".c-entry-content").text
-	    blurbs = blurb.split("\n")
-
-	    blurb_hold = []
-
-	    blurbs.collect do |entry|
-	     blurb_hold << entry.strip
-	    end
-
-	    blurb_hold.reject!(&:empty?)
-
-	    #Ahhhhh, need to get rid of promotional blurbs! - do have each item as a blurb!
-
-
-	       
+    # blurb[1].children.text - first blurb
+    
+    #Ahhhhh, need to get rid of promotional blurbs! - do have each item as a blurb!
+    
+    binding.pry
 	end
+	
 end
 
 Restaurant.scrape_heatmap
